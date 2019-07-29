ab84936608b15e1c129469c4a17dbbfd730c214f
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-07-29 17:44:44 +0200

    Done cloning dataset to TFDMemTable

--- src/Action.CreateMemTable.pas
+++ src/Action.CreateMemTable.pas
@@ -8,19 +8,18 @@ uses
   FireDAC.Comp.Client;
 
 type
-  TCreateMemTableAction = class (TComponent)
+  TCreateMemTableAction = class(TComponent)
   private
     FCode: TStrings;
   public
-    constructor Create (AOwner: TComponent); override;
+    constructor Create(AOwner: TComponent); override;
     destructor Destroy;
-    function CreateFDMemTable (dataSet: TDataSet): TFDMemTable;
-    procedure GenerateCode (dataSet: TDataSet);
+    function CreateFDMemTable(dataSet: TDataSet): TFDMemTable;
+    procedure GenerateCode(dataSet: TDataSet);
     property Code: TStrings read FCode write FCode;
   end;
 
 implementation
-
 
 constructor TCreateMemTableAction.Create(AOwner: TComponent);
 begin
@@ -33,23 +32,49 @@ begin
   FCode.Free;
 end;
 
-function TCreateMemTableAction.CreateFDMemTable (dataSet:TDataSet): TFDMemTable;
+function TCreateMemTableAction.CreateFDMemTable(dataSet: TDataSet): TFDMemTable;
+var
+  fld: TField;
 begin
+  dataSet.Open;
   Result := TFDMemTable.Create(Self);
-  with Result do
+  for fld in dataSet.Fields do
   begin
-    FieldDefs.Add('id', ftInteger);
-    FieldDefs.Add('text1', ftWideString, 30);
-    CreateDataSet;
-    InsertRecord([1, 'Ala ma kota']);
+    if fld.DataType in [ftAutoInc, ftInteger, ftWord, ftSmallint, ftLargeint,
+      ftBoolean, ftFloat, ftCurrency, ftDate, ftTime, ftDateTime] then
+      Result.FieldDefs.Add(fld.FieldName, fld.DataType)
+    else if (fld.DataType in [ftString, ftWideString]) and (fld.Size > 9999)
+    then
+      Result.FieldDefs.Add(fld.FieldName, fld.DataType, 100)
+    else if (fld.DataType in [ftString, ftWideString]) then
+      Result.FieldDefs.Add(fld.FieldName, fld.DataType, fld.DataSize)
+    else
+      Result.FieldDefs.Add(fld.FieldName, fld.DataType, fld.DataSize);
   end;
+  Result.CreateDataSet;
+  dataSet.DisableControls;
+  dataSet.First;
+  while not dataSet.Eof do
+  begin
+    Result.Insert;
+    for fld in dataSet.Fields do
+    begin
+      Result.FieldByName(fld.FieldName).Value :=
+        dataSet.FieldByName(fld.FieldName).Value;
+    end;
+    Result.Post;
+    dataSet.Next;
+  end;
+  Result.First;
+  dataSet.EnableControls;
 end;
 
 procedure TCreateMemTableAction.GenerateCode(dataSet: TDataSet);
 var
   sl: TStringList;
 begin
-  With Code do begin
+  With Code do
+  begin
     Clear;
     Add('ds := TFDMemTable.Create(AOwner);');
     Add('with ds do');
