59b15dd86780afce3a8d6a72b24d7e6cae48e7b5
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-07-29 19:15:14 +0200

    [doc] Added TFieldType values to the documentation

--- src/Form.Main.pas
+++ src/Form.Main.pas
@@ -60,6 +60,20 @@ function TFormMain.CreateSimpleMemTable: TFDMemTable;
 var
   ds: TFDMemTable;
 begin
+  (*
+  ---------------------------------------------------------------------------
+  [Doc]
+  TFieldType = (ftUnknown, ftString, ftSmallint, ftInteger, ftWord, // 0..4
+    ftBoolean, ftFloat, ftCurrency, ftBCD, ftDate, ftTime, ftDateTime, // 5..11
+    ftBytes, ftVarBytes, ftAutoInc, ftBlob, ftMemo, ftGraphic, ftFmtMemo, // 12..18
+    ftParadoxOle, ftDBaseOle, ftTypedBinary, ftCursor, ftFixedChar, ftWideString, // 19..24
+    ftLargeint, ftADT, ftArray, ftReference, ftDataSet, ftOraBlob, ftOraClob, // 25..31
+    ftVariant, ftInterface, ftIDispatch, ftGuid, ftTimeStamp, ftFMTBcd, // 32..37
+    ftFixedWideChar, ftWideMemo, ftOraTimeStamp, ftOraInterval, // 38..41
+    ftLongWord, ftShortint, ftByte, ftExtended, ftConnection, ftParams, ftStream, //42..48
+    ftTimeStampOffset, ftObject, ftSingle); //49..51
+  ---------------------------------------------------------------------------
+  *)
   ds := TFDMemTable.Create(Self);
   with ds do
   begin
