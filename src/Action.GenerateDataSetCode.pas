e80db910eacbf91c9c5fa97a9249a01f3211e3a9
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-07-30 18:18:54 +0200

    [Doc] Moved TFieldType values from main

--- src/Action.GenerateDataSetCode.pas
+++ src/Action.GenerateDataSetCode.pas
@@ -46,7 +46,21 @@ end;
 
 function TGenDataSetCodeAction.GenCodeLineFieldDefAdd(fld: TField): string;
 begin
-  if fld.DataType in [ftAutoInc, ftInteger, ftWord, ftSmallint, ftLargeint,
+   (*
+  ---------------------------------------------------------------------------
+  [Doc]
+  TFieldType = (ftUnknown, ftString, ftSmallint, ftInteger, ftWord, // 0..4
+    ftBoolean, ftFloat, ftCurrency, ftBCD, ftDate, ftTime, ftDateTime, // 5..11
+    ftBytes, ftVarBytes, ftAutoInc, ftBlob, ftMemo, ftGraphic, ftFmtMemo, // 12..18
+    ftParadoxOle, ftDBaseOle, ftTypedBinary, ftCursor, ftFixedChar, ftWideString, // 19..24
+    ftLargeint, ftADT, ftArray, ftReference, ftDataSet, ftOraBlob, ftOraClob, // 25..31
+    ftVariant, ftInterface, ftIDispatch, ftGuid, ftTimeStamp, ftFMTBcd, // 32..37
+    ftFixedWideChar, ftWideMemo, ftOraTimeStamp, ftOraInterval, // 38..41
+    ftLongWord, ftShortint, ftByte, ftExtended, ftConnection, ftParams, ftStream, //42..48
+    ftTimeStampOffset, ftObject, ftSingle); //49..51
+  ---------------------------------------------------------------------------
+  *)
+ if fld.DataType in [ftAutoInc, ftInteger, ftWord, ftSmallint, ftLargeint,
     ftBoolean, ftFloat, ftCurrency, ftDate, ftTime, ftDateTime] then
     Result := 'FieldDefs.Add(' + QuotedStr(fld.FieldName) + ', ' +
       FieldTypeToString(fld.DataType) + ');'
