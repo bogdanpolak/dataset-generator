a1069be00afab8007ffdce2b898fe44358747627
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-01-04 19:37:15 +0000

    Formatted code

--- src/Comp.Generator.DataSetCode.pas
+++ src/Comp.Generator.DataSetCode.pas
@@ -1,12 +1,10 @@
-﻿{ * ------------------------------------------------------------------------
-  * ♥
-  * ♥ DataSet to Delphi Code (create TFDMemTable with the data)
-  * ♥
-  * Component: TGenerateDataSetCode
-  * Project: https://github.com/bogdanpolak/datasetToDelphiCode
-  * ReleaseDate: ↓ see below in the component const section ↓
-  * ReleaseVersion: ↓ see below in the component const section ↓
-  * ------------------------------------------------------------------------ }
+﻿{* ------------------------------------------------------------------------
+ * ♥
+ * ♥ DataSet to Delphi Code (create TFDMemTable with the data)
+ * ♥
+ * Component: TGenerateDataSetCode
+ * Project: https://github.com/bogdanpolak/datasetToDelphiCode
+ * ------------------------------------------------------------------------ }
 
 unit Comp.Generator.DataSetCode;
 
@@ -96,20 +94,18 @@ end;
 
 function TGenerateDataSetCode.GenCodeLineFieldDefAdd(fld: TField): string;
 begin
-  (*
-    ---------------------------------------------------------------------------
+  (* -----------------------------------------------------------------------
     [Doc]
-    TFieldType = (ftUnknown, ftString, ftSmallint, ftInteger, ftWord, // 0..4
-    ftBoolean, ftFloat, ftCurrency, ftBCD, ftDate, ftTime, ftDateTime, // 5..11
-    ftBytes, ftVarBytes, ftAutoInc, ftBlob, ftMemo, ftGraphic, ftFmtMemo, // 12..18
-    ftParadoxOle, ftDBaseOle, ftTypedBinary, ftCursor, ftFixedChar, ftWideString, // 19..24
-    ftLargeint, ftADT, ftArray, ftReference, ftDataSet, ftOraBlob, ftOraClob, // 25..31
-    ftVariant, ftInterface, ftIDispatch, ftGuid, ftTimeStamp, ftFMTBcd, // 32..37
-    ftFixedWideChar, ftWideMemo, ftOraTimeStamp, ftOraInterval, // 38..41
-    ftLongWord, ftShortint, ftByte, ftExtended, ftConnection, ftParams, ftStream, //42..48
-    ftTimeStampOffset, ftObject, ftSingle); //49..51
-    ---------------------------------------------------------------------------
-  *)
+   TFieldType = ( ftUnknown, ftString, ftSmallint, ftInteger, ftWord,
+   ftBoolean, ftFloat, ftCurrency, ftBCD, ftDate, ftTime, ftDateTime,
+   ftBytes, ftVarBytes, ftAutoInc, ftBlob, ftMemo, ftGraphic, ftFmtMemo,
+   ftParadoxOle, ftDBaseOle, ftTypedBinary, ftCursor, ftFixedChar, ftWideString,
+   ftLargeint, ftADT, ftArray, ftReference, ftDataSet, ftOraBlob, ftOraClob,
+   ftVariant, ftInterface, ftIDispatch, ftGuid, ftTimeStamp, ftFMTBcd,
+   ftFixedWideChar, ftWideMemo, ftOraTimeStamp, ftOraInterval,
+   ftLongWord, ftShortint, ftByte, ftExtended, ftConnection, ftParams, ftStream,
+   ftTimeStampOffset, ftObject, ftSingle);
+   ------------------------------------------------------------------------- *)
   if fld.DataType in [ftAutoInc, ftInteger, ftWord, ftSmallint, ftLargeint,
     ftBoolean, ftFloat, ftCurrency, ftDate, ftTime, ftDateTime] then
     Result := 'FieldDefs.Add(' + QuotedStr(fld.FieldName) + ', ' +
