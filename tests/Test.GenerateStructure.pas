5578e0e7ad31e2404baa5eb8c0d6d01040bd28b2
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-01-06 21:41:46 +0000

    Moved test

--- tests/Test.GenerateStructure.pas
+++ tests/Test.GenerateStructure.pas
@@ -30,18 +30,17 @@ type
     [TearDown]
     procedure TearDown;
   published
-    // -------------
     procedure GenFieldDef_Integer;
     procedure GenFieldDef_WideString;
     procedure GenFieldDef_Date;
     procedure GenFieldDef_DateTime;
     procedure GenFieldDef_BCD;
     // -------------
+    procedure TestSample1;
+    // -------------
     procedure Test_IndentationText_BCDField;
     procedure Test_Indentation_Empty;
     procedure Test_Indentation_1Space;
-    // -------------
-    procedure TestSample1;
   end;
 
 implementation
@@ -158,6 +157,55 @@ begin
 end;
 
 // -----------------------------------------------------------------------
+// Tests for: Sample1
+// -----------------------------------------------------------------------
+
+function TestGenerateStructure.GivenSampleDataSetWithTwoRows(aOwner: TComponent)
+  : TDataSet;
+var
+  memTable: TFDMemTable;
+begin
+  memTable := TFDMemTable.Create(aOwner);
+  with memTable do
+  begin
+    FieldDefs.Add('id', ftInteger);
+    FieldDefs.Add('text1', ftWideString, 30);
+    FieldDefs.Add('date1', ftDate);
+    FieldDefs.Add('float1', ftFloat);
+    FieldDefs.Add('currency1', ftCurrency);
+    CreateDataSet;
+    AppendRecord([1, 'Alice has a cat', EncodeDate(2019, 09, 16), 1.2, 1200]);
+    AppendRecord([2, 'Eva has a dog', System.Variants.Null, Null, 950]);
+    First;
+  end;
+  Result := memTable;
+end;
+
+procedure TestGenerateStructure.TestSample1;
+var
+  actualCode: string;
+  expectedCode: string;
+begin
+  fGenerator.DataSet := GivenSampleDataSetWithTwoRows(fOwner);
+
+  fGenerator.Execute;
+  actualCode := fGenerator.CodeWithStructure.Text;
+
+  expectedCode := //.
+    '  ds := TFDMemTable.Create(AOwner);'#13 + //.
+    '  with ds do'#13 + //.
+    '  begin'#13 + //.
+    '    FieldDefs.Add(''id'', ftInteger);'#13 + //.
+    '    FieldDefs.Add(''text1'', ftWideString, 30);'#13 + //.
+    '    FieldDefs.Add(''date1'', ftDate);'#13 + //.
+    '    FieldDefs.Add(''float1'', ftFloat);'#13 + //.
+    '    FieldDefs.Add(''currency1'', ftCurrency);'#13 + //.
+    '    CreateDataSet;'#13 + //.
+    '  end;'#13;
+  Assert.AreMemosEqual(expectedCode, actualCode);
+end;
+
+// -----------------------------------------------------------------------
 // Tests for: property IndentationText
 // -----------------------------------------------------------------------
 
@@ -244,55 +292,6 @@ begin
     '    with FieldDefs.AddFieldDef do begin'#13 + //.
     '      Name := ''Bugdet'';  DataType := ftBCD;  Precision := 11;  Size := 3;'#13
     + '    end;'#13 + //.
-    '    CreateDataSet;'#13 + //.
-    '  end;'#13;
-  Assert.AreMemosEqual(expectedCode, actualCode);
-end;
-
-// -----------------------------------------------------------------------
-// Tests for: Sample1
-// -----------------------------------------------------------------------
-
-function TestGenerateStructure.GivenSampleDataSetWithTwoRows(aOwner: TComponent)
-  : TDataSet;
-var
-  memTable: TFDMemTable;
-begin
-  memTable := TFDMemTable.Create(aOwner);
-  with memTable do
-  begin
-    FieldDefs.Add('id', ftInteger);
-    FieldDefs.Add('text1', ftWideString, 30);
-    FieldDefs.Add('date1', ftDate);
-    FieldDefs.Add('float1', ftFloat);
-    FieldDefs.Add('currency1', ftCurrency);
-    CreateDataSet;
-    AppendRecord([1, 'Alice has a cat', EncodeDate(2019, 09, 16), 1.2, 1200]);
-    AppendRecord([2, 'Eva has a dog', System.Variants.Null, Null, 950]);
-    First;
-  end;
-  Result := memTable;
-end;
-
-procedure TestGenerateStructure.TestSample1;
-var
-  actualCode: string;
-  expectedCode: string;
-begin
-  fGenerator.DataSet := GivenSampleDataSetWithTwoRows(fOwner);
-
-  fGenerator.Execute;
-  actualCode := fGenerator.CodeWithStructure.Text;
-
-  expectedCode := //.
-    '  ds := TFDMemTable.Create(AOwner);'#13 + //.
-    '  with ds do'#13 + //.
-    '  begin'#13 + //.
-    '    FieldDefs.Add(''id'', ftInteger);'#13 + //.
-    '    FieldDefs.Add(''text1'', ftWideString, 30);'#13 + //.
-    '    FieldDefs.Add(''date1'', ftDate);'#13 + //.
-    '    FieldDefs.Add(''float1'', ftFloat);'#13 + //.
-    '    FieldDefs.Add(''currency1'', ftCurrency);'#13 + //.
     '    CreateDataSet;'#13 + //.
     '  end;'#13;
   Assert.AreMemosEqual(expectedCode, actualCode);
