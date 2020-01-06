89b2ab6dcdd6e1f023914a6a98df1a82bc8db937
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-01-06 21:39:28 +0000

    Refactored other structure tests

--- tests/Test.GenerateStructure.pas
+++ tests/Test.GenerateStructure.pas
@@ -21,10 +21,7 @@ type
   private
     fGenerator: TDSGeneratorUnderTest;
     fOwner: TComponent;
-    fExpectedCode: TStringList;
-    procedure AreCodesEqual(expectedCode: TStrings; actualCode: TStrings);
     function GivenSampleDataSetWithTwoRows(aOwner: TComponent): TDataSet;
-    function ReplaceArrowsAndDiamonds(const s: String): string;
     function GivenField(aOwner: TComponent; const fieldName: string;
       fieldType: TFieldType; size: integer = 0): TField;
   public
@@ -41,7 +38,6 @@ type
     procedure GenFieldDef_BCD;
     // -------------
     procedure Test_IndentationText_BCDField;
-    procedure Test_Indentation_MultilineTextValue;
     procedure Test_Indentation_Empty;
     procedure Test_Indentation_1Space;
     // -------------
@@ -55,48 +51,6 @@ uses
   Data.FmtBcd;
 
 // -----------------------------------------------------------------------
-// Templates
-// -----------------------------------------------------------------------
-
-const
-  CodeTemplateOneBcdField =
-  (* *) '◇ds := TFDMemTable.Create(AOwner);→' +
-  (* *) '◇with ds do→' +
-  (* *) '◇begin→' +
-  (* *) '◇◇with FieldDefs.AddFieldDef do begin→' +
-  (* *) '◇◇◇Name := ''%s'';  DataType := %s;  Precision := %d;  Size := %d;→' +
-  (* *) '◇◇end;→' +
-  (* *) '◇◇CreateDataSet;→' +
-  (* *) '◇end;→';
-
-  CodeTemplateOneField =
-  (* *) '◇ds := TFDMemTable.Create(AOwner);→' +
-  (* *) '◇with ds do→' +
-  (* *) '◇begin→' +
-  (* *) '◇◇FieldDefs.Add(''f1'', %s);→' +
-  (* *) '◇◇CreateDataSet;→' +
-  (* *) '◇end;→';
-
-
-  // -----------------------------------------------------------------------
-  // Utils section
-  // -----------------------------------------------------------------------
-
-function TestGenerateStructure.ReplaceArrowsAndDiamonds
-  (const s: String): string;
-begin
-  Result := StringReplace(s, '→', #13#10, [rfReplaceAll]);
-  Result := StringReplace(Result, '◇', fGenerator.IndentationText,
-    [rfReplaceAll])
-end;
-
-procedure TestGenerateStructure.AreCodesEqual(expectedCode: TStrings;
-  actualCode: TStrings);
-begin
-  Assert.AreEqual(expectedCode.Text, actualCode.Text);
-end;
-
-// -----------------------------------------------------------------------
 // Setup and TearDown section
 // -----------------------------------------------------------------------
 
@@ -104,14 +58,12 @@ procedure TestGenerateStructure.Setup;
 begin
   fGenerator := TDSGeneratorUnderTest.Create(nil);
   fOwner := TComponent.Create(nil);
-  fExpectedCode := TStringList.Create;
 end;
 
 procedure TestGenerateStructure.TearDown;
 begin
   fGenerator.Free;
   fOwner.Free;
-  fExpectedCode.Free;
 end;
 
 
@@ -210,30 +162,40 @@ end;
 // -----------------------------------------------------------------------
 
 procedure TestGenerateStructure.Test_Indentation_1Space;
+var
+  actualCode: string;
+  expectedCode: string;
 begin
   fGenerator.DataSet := TFDMemTable.Create(fOwner);
   with fGenerator.DataSet as TFDMemTable do
   begin
-    FieldDefs.Add('f1', ftInteger);
+    FieldDefs.Add('Points', ftInteger);
     CreateDataSet;
-    AppendRecord([1]);
-    First;
   end;
   fGenerator.IndentationText := ' ';
 
   fGenerator.Execute;
+  actualCode := fGenerator.CodeWithStructure.Text;
 
-  fExpectedCode.Text := ReplaceArrowsAndDiamonds(Format(CodeTemplateOneField,
-    ['ftInteger', '1']));
-  AreCodesEqual(fExpectedCode, fGenerator.CodeWithStructure);
+  expectedCode := //.
+    ' ds := TFDMemTable.Create(AOwner);'#13 + //.
+    ' with ds do'#13 + //.
+    ' begin'#13 + //.
+    '  FieldDefs.Add(''Points'', ftInteger);'#13 + //.
+    '  CreateDataSet;'#13 + //.
+    ' end;'#13;
+  Assert.AreMemosEqual(expectedCode, actualCode);
 end;
 
 procedure TestGenerateStructure.Test_Indentation_Empty;
+var
+  actualCode: string;
+  expectedCode: string;
 begin
   fGenerator.DataSet := TFDMemTable.Create(fOwner);
   with fGenerator.DataSet as TFDMemTable do
   begin
-    FieldDefs.Add('f1', ftInteger);
+    FieldDefs.Add('Points', ftInteger);
     CreateDataSet;
     AppendRecord([1]);
     First;
@@ -241,64 +203,50 @@ begin
   fGenerator.IndentationText := '';
 
   fGenerator.Execute;
+  actualCode := fGenerator.CodeWithStructure.Text;
 
-  fExpectedCode.Text := ReplaceArrowsAndDiamonds(Format(CodeTemplateOneField,
-    ['ftInteger', '1']));
-  AreCodesEqual(fExpectedCode, fGenerator.CodeWithStructure);
-end;
-
-procedure TestGenerateStructure.Test_Indentation_MultilineTextValue;
-var
-  aExpectedFieldValue: string;
-begin
-  fGenerator.DataSet := TFDMemTable.Create(fOwner);
-  with fGenerator.DataSet as TFDMemTable do
-  begin
-    FieldDefs.Add('f1', ftWideString, 300);
-    CreateDataSet;
-    AppendRecord(['Covers Dependency Injection, you''ll learn about' +
-      ' Constructor Injection, Property Injection, and Method Injection' +
-      ' and about the right and wrong way to use it']);
-    First;
-  end;
-  fGenerator.IndentationText := '  ';
-
-  fGenerator.Execute;
-
-  aExpectedFieldValue := '→◇◇◇' +
-    QuotedStr(
-    'Covers Dependency Injection, you''ll learn about Constructor Injecti') +
-    '+→◇◇◇' + QuotedStr
-    ('on, Property Injection, and Method Injection and about the right and') +
-    '+→◇◇◇' + QuotedStr(' wrong way to use it');
-  fExpectedCode.Text := ReplaceArrowsAndDiamonds(Format(CodeTemplateOneField,
-    ['ftWideString, 300', aExpectedFieldValue]));
-  AreCodesEqual(fExpectedCode, fGenerator.CodeWithStructure);
+  expectedCode := //.
+    'ds := TFDMemTable.Create(AOwner);'#13 + //.
+    'with ds do'#13 + //.
+    'begin'#13 + //.
+    'FieldDefs.Add(''Points'', ftInteger);'#13 + //.
+    'CreateDataSet;'#13 + //.
+    'end;'#13;
+  Assert.AreMemosEqual(expectedCode, actualCode);
 end;
 
 procedure TestGenerateStructure.Test_IndentationText_BCDField;
+var
+  actualCode: string;
+  expectedCode: string;
 begin
   fGenerator.DataSet := TFDMemTable.Create(fOwner);
   with fGenerator.DataSet as TFDMemTable do
   begin
     with FieldDefs.AddFieldDef do
     begin
-      Name := 'xyz123';
+      Name := 'Bugdet';
       DataType := ftBcd;
-      Precision := 8;
-      size := 2;
+      Precision := 11;
+      size := 3;
     end;
     CreateDataSet;
-    AppendRecord([1.01]);
-    First;
   end;
   fGenerator.IndentationText := '  ';
 
   fGenerator.Execute;
+  actualCode := fGenerator.CodeWithStructure.Text;
 
-  fExpectedCode.Text := ReplaceArrowsAndDiamonds(Format(CodeTemplateOneBcdField,
-    ['xyz123', 'ftBCD', 8, 2]));
-  AreCodesEqual(fExpectedCode, fGenerator.CodeWithStructure);
+  expectedCode := //.
+    '  ds := TFDMemTable.Create(AOwner);'#13 + //.
+    '  with ds do'#13 + //.
+    '  begin'#13 + //.
+    '    with FieldDefs.AddFieldDef do begin'#13 + //.
+    '      Name := ''Bugdet'';  DataType := ftBCD;  Precision := 11;  Size := 3;'#13
+    + '    end;'#13 + //.
+    '    CreateDataSet;'#13 + //.
+    '  end;'#13;
+  Assert.AreMemosEqual(expectedCode, actualCode);
 end;
 
 // -----------------------------------------------------------------------
@@ -327,23 +275,27 @@ begin
 end;
 
 procedure TestGenerateStructure.TestSample1;
+var
+  actualCode: string;
+  expectedCode: string;
 begin
   fGenerator.DataSet := GivenSampleDataSetWithTwoRows(fOwner);
 
   fGenerator.Execute;
+  actualCode := fGenerator.CodeWithStructure.Text;
 
-  fExpectedCode.Text := ReplaceArrowsAndDiamonds(
-    (* *) '◇ds := TFDMemTable.Create(AOwner);→' +
-    (* *) '◇with ds do→' +
-    (* *) '◇begin→' +
-    (* *) '◇◇FieldDefs.Add(''id'', ftInteger);→' +
-    (* *) '◇◇FieldDefs.Add(''text1'', ftWideString, 30);→' +
-    (* *) '◇◇FieldDefs.Add(''date1'', ftDate);→' +
-    (* *) '◇◇FieldDefs.Add(''float1'', ftFloat);→' +
-    (* *) '◇◇FieldDefs.Add(''currency1'', ftCurrency);→' +
-    (* *) '◇◇CreateDataSet;→' +
-    (* *) '◇end;→');
-  AreCodesEqual(fExpectedCode, fGenerator.CodeWithStructure);
+  expectedCode := //.
+    '  ds := TFDMemTable.Create(AOwner);'#13 + //.
+    '  with ds do'#13 + //.
+    '  begin'#13 + //.
+    '    FieldDefs.Add(''id'', ftInteger);'#13 + //.
+    '    FieldDefs.Add(''text1'', ftWideString, 30);'#13 + //.
+    '    FieldDefs.Add(''date1'', ftDate);'#13 + //.
+    '    FieldDefs.Add(''float1'', ftFloat);'#13 + //.
+    '    FieldDefs.Add(''currency1'', ftCurrency);'#13 + //.
+    '    CreateDataSet;'#13 + //.
+    '  end;'#13;
+  Assert.AreMemosEqual(expectedCode, actualCode);
 end;
 
 initialization
