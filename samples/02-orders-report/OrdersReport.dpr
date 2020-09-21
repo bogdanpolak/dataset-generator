6742652a2f1af2036248215661b215215fa22fb3
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-09-21 23:40:54 +0200

    Added new VclForms project

---
+++ samples/02-orders-report/OrdersReport.dpr
@@ -0,0 +1,14 @@
+program OrdersReport;
+
+uses
+  Vcl.Forms,
+  Form.Main in 'Form.Main.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
