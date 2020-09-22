19319296fc8788413a4b23388b0224525ff75db9
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-09-22 22:08:03 +0200

    Added TForm1.TestMonths();

--- samples/02-orders-report/Form.Main.pas
+++ samples/02-orders-report/Form.Main.pas
@@ -13,14 +13,14 @@ uses
   Vcl.Graphics,
   Vcl.Controls,
   Vcl.Forms,
-  Vcl.Dialogs;
+  Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    procedure FormShow(Sender: TObject);
   private
-    { Private declarations }
+    procedure TestMonths;
   public
-    { Public declarations }
   end;
 
 var
@@ -29,5 +29,36 @@ var
 implementation
 
 {$R *.dfm}
+
+uses
+  Spring,
+  Spring.Collections,
+
+  Data.DataModule1;
+
+
+procedure TForm1.TestMonths();
+var
+  aListBox: TListBox;
+  activeMonths: IList<Tuple<String, Word, Word>>;
+  aMonth: Tuple<String, Word, Word>;
+begin
+  aListBox := TListBox.Create(self);
+  aListBox.Align := alLeft;
+  aListBox.AlignWithMargins := True;
+  aListBox.Parent := Self;
+  DataModule1.Connect();
+  activeMonths := DataModule1.GetActiveMonths();
+  aListBox.Clear;
+  for aMonth in activeMonths do
+  begin
+    aListBox.AddItem(aMonth.Value1,nil);
+  end;
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  TestMonths();
+end;
 
 end.
