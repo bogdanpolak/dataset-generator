585f2692aa441f1fd87c1a29c39afa3b65df2d67
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-09-23 22:47:56 +0200

    Added Scorecards logic

--- samples/02-employee-scorecards/Logic.Scorecards.pas
+++ samples/02-employee-scorecards/Logic.Scorecards.pas
@@ -4,8 +4,34 @@ interface
 
 uses
   System.Classes,
-  System.SysUtils;
+  System.SysUtils,
+  System.DateUtils,
+  Spring.Collections,
+  {-}
+  Data.DataModule1;
+
+type
+  TEmployeeScore = class
+
+  end;
+
+  TScorecards = record
+  private
+    fYear: Integer;
+    fMonth: Integer;
+  public
+    fEmployeeScores: IList<TEmployeeScore>;
+    constructor Create(aYear: word; aMonth: word);
+  end;
 
 implementation
+
+
+constructor TScorecards.Create(aYear: word; aMonth: word);
+begin
+  fEmployeeScores := Spring.Collections.TCollections.CreateList<TEmployeeScore>( True);
+  fYear := aYear;
+  fMonth := aMonth;
+end;
 
 end.
