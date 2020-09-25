dea4631bf39dd597342157a8467beec514e7c218
Bogdan Polak <bogdan.polak@bsc.com.pl>
2020-09-25 23:50:53 +0200

    Done TScorecards.FillUsingDetails

--- samples/02-employee-scorecards/Logic.Scorecards.pas
+++ samples/02-employee-scorecards/Logic.Scorecards.pas
@@ -5,9 +5,11 @@ interface
 uses
   System.Classes,
   System.SysUtils,
+  System.StrUtils,
   System.DateUtils,
-  Spring.Collections,
+  System.Math,
   Data.DB,
+  Spring.Collections,
   {-}
   Data.DataModule1;
 
@@ -17,6 +19,7 @@ type
     fMonth: TDateTime;
     fEmployeeName: String;
     fOrderCount: Integer;
+    fOrderValues: string;
     fMaxScore: Integer;
     fFinalScore: Integer;
   end;
@@ -76,9 +79,58 @@ begin
   end;
 end;
 
-procedure TScorecards.FillUsingDetails(const dsDetails: TDataSet);
+function CountOrderScore(const aTotalOrderValue: Currency): Integer;
 begin
+  Result := 0;
+end;
 
+procedure TScorecards.FillUsingDetails(const dsDetails: TDataSet);
+var
+  employeeId: Integer;
+  currentOrderID: Integer;
+  unitPrice: Currency;
+  quantity: Integer;
+  discount: Double;
+  orderID: Integer;
+  strOrderValues: String;
+  itemTotal: Currency;
+  totalOrderValue: Currency;
+  score: TEmployeeScore;
+begin
+  for employeeId in fEmployeeScores.Keys do
+  begin
+    dsDetails.Locate('EmployeeId', employeeId, []);
+    currentOrderID := 0;
+    totalOrderValue := 0;
+    strOrderValues := '';
+    while not(dsDetails.Eof) and
+        (dsDetails.FieldByName('EmployeeId').AsInteger = employeeId) do
+    begin
+      orderID := dsDetails.FieldByName('OrderId').AsInteger;
+      unitPrice := dsDetails.FieldByName('UnitPrice').AsCurrency;
+      quantity := dsDetails.FieldByName('Quantity').AsInteger;
+      discount := dsDetails.FieldByName('Discount').AsFloat;
+      itemTotal := RoundTo(unitPrice * quantity * (1 - discount), -2);
+      if orderID <> currentOrderID then
+      begin
+        if (totalOrderValue > 0) then
+        begin
+          strOrderValues := strOrderValues + '| ' + CurrToStrF(totalOrderValue,
+              ffFixed, 2);
+          // CountOrderScore(totalOrderValue);
+          totalOrderValue := 0;
+        end;
+        currentOrderID := orderID;
+      end;
+      totalOrderValue := totalOrderValue + itemTotal;
+      dsDetails.Next;
+    end;
+    if (Length(strOrderValues) > 0) and (fEmployeeScores.TryGetValue(employeeId, score))
+    then
+    begin
+      score.fOrderValues := strOrderValues.Substring(2);
+    end;
+  end;
 end;
 
 constructor TScorecards.Create(aYear: word; aMonth: word);
