71843a34bae32422aee144c92856ddc3241d939a
Bogdan Polak <bogdanpol@gmail.com>
2020-09-27 18:00:54 +0200

    step 1 - refactoring

--- tests/Test.TextWrapper.pas
+++ tests/Test.TextWrapper.pas
@@ -25,14 +25,18 @@ implementation
 uses
   Comp.Generator.DataSetCode;
 
+//  ---------1---------2---------3---------4---------5---------6
+//  123456789012345678901234567890123456789012345678901234567890
+//  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
 procedure TTestTextWrapper.NoWrapText;
 var
   actual: TArray<string>;
 begin
   fText := 'Lorem ipsum ...';
-  actual := TTextWrapper.WrapTextWholeWords(fText,20);
-  Assert.AreEqual(1,Length(actual));
-  Assert.AreEqual(fText,actual[0]);
+  actual := TTextWrapper.WrapTextWholeWords(fText, 20);
+  Assert.AreEqual(1, Length(actual));
+  Assert.AreEqual(fText, actual[0]);
 end;
 
 end.
